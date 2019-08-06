f4f5960ef8717a9837ce2136fc088a19fcaa6e08
Michael Cooper <michaelcooper99@hotmail.com>
2019-08-05 23:56:24 -0400

    import re for hexify function; all known cases covered.

--- HexSpeak.py
+++ HexSpeak.py
@@ -2,6 +2,7 @@
 #   August 2019
 #   hexspeak a file
 
+import re
 
 def main():
     try:
@@ -13,17 +14,20 @@ def main():
     finally:
         toHex.close()
 
-    for line in lines:
-       print(line)
-
     hexify(lines)
 
-def hexify():
-    pass
+def hexify(lines):
+    for line in lines:
+        line = re.sub(r"(O|o)", "0", line)
+        line = re.sub(r"(I|L|i|l)", "1", line)
+        line = re.sub(r"(S|s)", "5", line)
+        line = re.sub(r"(G)", "6", line)
+        line = re.sub(r"(g)", "9", line)
+        line = re.sub(r"(ate)", "8", line, re.IGNORECASE)
+        line = re.sub(r"(T|t)", "7", line)
+        print(line)
     # if name contains forbidden letters i.e. only has ABCDEF
     # replace O -> zero, I | L -> one, S -> 5, T -> 7, G -> 6, g -> 9, ate -> 8 | a7e
-
-
 
 if __name__ == "__main__":
     main()
