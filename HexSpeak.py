c7dbacdb15c40fc750e8856354904974db0f8c28
Michael Cooper <michaelcooper99@hotmail.com>
2019-08-06 01:33:58 -0400

    compiled regexes; strange bug that allows some characters to not be noticed by regex

--- HexSpeak.py
+++ HexSpeak.py
@@ -17,15 +17,36 @@ def main():
     hexify(lines)
 
 def hexify(lines):
+
+    # compile regexes
+    res = []
+    res.append(re.compile("[hjkmnpqruvwxyz]"))
+    res.append(re.compile(r"(O|o)",))
+    res.append(re.compile(r"(I|L|i|l)"))
+    res.append(re.compile(r"(S|s)"))
+    res.append(re.compile(r"(G)"))
+    res.append(re.compile(r"(g)"))
+    res.append(re.compile(r"(ate)"))
+    res.append(re.compile(r"(T|t)"))
+
+    newlines = []
+
     for line in lines:
-        line = re.sub(r"(O|o)", "0", line)
-        line = re.sub(r"(I|L|i|l)", "1", line)
-        line = re.sub(r"(S|s)", "5", line)
-        line = re.sub(r"(G)", "6", line)
-        line = re.sub(r"(g)", "9", line)
-        line = re.sub(r"(ate)", "8", line, re.IGNORECASE)
-        line = re.sub(r"(T|t)", "7", line)
-        print(line)
+        # remove line from list if it contains illegal characters that can not be converted
+        match = res[0].search(line, re.IGNORECASE)
+        if not match:
+            line = res[1].sub("0", line)
+            line = res[2].sub("1", line)
+            line = res[3].sub("5", line)
+            line = res[4].sub("6", line)
+            line = res[5].sub("9", line)
+            line = res[6].sub("8", line, re.IGNORECASE)
+            line = res[7].sub("7", line)
+            newlines.append(line)
+
+        with open(".//outHex.txt", "w+") as f:
+            all = '\n'.join(newlines)
+            f.write(all)
     # if name contains forbidden letters i.e. only has ABCDEF
     # replace O -> zero, I | L -> one, S -> 5, T -> 7, G -> 6, g -> 9, ate -> 8 | a7e
 
